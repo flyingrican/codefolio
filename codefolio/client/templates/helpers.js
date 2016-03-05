005316a191a1da71245d868f7481ca4164606b7d
flyingrican <kortiz1514@gmail.com>
2016-03-04 19:39:15 -0500

    commit

---
+++ codefolio/client/templates/helpers.js
@@ -0,0 +1,34 @@
+Template.login.events({
+    'submit .login-user': function(event){
+        event.preventDefault();
+        var username = event.target.username.value;
+        var password = event.target.password.value;
+
+        Meteor.loginWithPassword(username, password, function(err){
+            if (err) {
+                event.target.username.value = username;
+                event.target.password.value = password;
+                FlashMessages.sendError(err.reason);
+            }else{
+                FlashMessages.sendSuccess('You are now logged in!');
+                Router.go('/admin/projects');
+            }
+        });
+        event.target.username.value = '';
+        event.target.password.value = '';
+    }
+});
+
+Template.layout.events({
+    'click .logout-user': function(event){
+        event.preventDefault();
+        Meteor.logout(function(err){
+            if (err) {
+                FlashMessages.sendError(err.reason);
+            } else {
+                FlashMessages.sendSuccess('You are now logged out!');
+                Router.go('/');
+            }
+        });
+    }
+});
